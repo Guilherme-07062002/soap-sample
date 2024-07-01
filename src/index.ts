bd9e62045d879fc2c709447343472dbeda0fd74f
Guilherme-07062002 <guilherme762002@gmail.com>
2024-07-01 15:00:52 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import { callDivideService } from './send-request';
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/divide', async (req, res) => {
+  // Exemplo de uso
+  const result = await callDivideService(25, 5)
+    .then(result => {
+      console.log('Resultado da divisão:', result);
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+
+  res.send('Resultado da divisão: ' + result);
+})
+
+server.listen(3000, () => {
+  console.log('\nlistening on 3000');
+});
+
---
+++ src/send-request.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { parseStringPromise } from 'xml2js';
+
+export async function callDivideService(intA: number, intB: number) {
+    const url = 'http://www.dneonline.com/calculator.asmx';
+    const soapEnvelope = `
+    <?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <Divide xmlns="http://tempuri.org/">
+          <intA>${intA}</intA>
+          <intB>${intB}</intB>
+        </Divide>
+      </soap:Body>
+    </soap:Envelope>
+  `;
+
+    console.log('Calling SOAP service:', url);
+    const response = await axios.post(url, soapEnvelope, {
+        headers: {
+            'Content-Type': 'text/xml; charset=utf-8',
+            'SOAPAction': 'http://tempuri.org/Divide',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+        },
+    }).then(async response => {
+        const result = await parseStringPromise(response.data, { explicitArray: false });
+        const divideResult = result['soap:Envelope']['soap:Body']['DivideResponse']['DivideResult'];
+
+        return parseFloat(divideResult);
+    }).catch(error => {
+        console.error('Erro:', error);
+        return error;
+    });
+}
+
+
