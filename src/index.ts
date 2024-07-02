99b07d5e416d3820b236d0a31458ff901b76cf1d
Guilherme-07062002 <guilherme762002@gmail.com>
2024-07-02 20:36:24 -0300

    refactor: Add SOAP mock endpoint and set response content type to XML

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,31 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.use((req, res, next) => {
+  res.header('Content-Type', 'text/xml');
+  next();
+});
+
+app.post('/mock-soap', (req, res) => {
+  // XML de resposta mockado
+  const responseXml = `
+    <Response>
+      <Message>Success</Message>
+      <Data>
+        <Item>
+          <Name>Item 1</Name>
+          <Value>123</Value>
+        </Item>
+        <Item>
+          <Name>Item 2</Name>
+          <Value>456</Value>
+        </Item>
+      </Data>
+    </Response>
+  `;
+  res.send(responseXml);
+});
+
 app.post('/divide', async (req, res) => {
   const body = req.body;
   const intA = parseInt(body.intA);
